ce18b7d82bf95e1a88bb2c82512c5b8b9fa246e6
nianod <arnoldkk422@gmail.com>
2024-12-08 22:56:11 +0300

    my commit

---
+++ project.js
@@ -0,0 +1,17 @@
+// deposit some money
+//determine number pf lines to bet
+//collect a bet amount
+//spin slot machine
+//check if the user won
+//give the user their winnings
+//play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter deposit amount: ");
+    constnumberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount)) 
+};
+deposit();
