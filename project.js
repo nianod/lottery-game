63d0441382d7ae949fdeae78708d60d799fa6080
nianod <arnoldkk422@gmail.com>
2024-12-09 13:52:54 +0300

    my commit

--- project.js
+++ project.js
@@ -9,9 +9,33 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-    const depositAmount = prompt("Enter deposit amount: ");
-    constnumberDepositAmount = parseFloat(depositAmount);
-
-    if (isNaN(numberDepositAmount)) 
+    while (true) {
+        const depositAmount = prompt("Enter deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+    
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0 ) {
+            console.log("invalid deposit amount, try again dude!")
+        } else {
+            return numberDepositAmount
+        }
+    }
+  
 };
-deposit();
+
+ const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+    
+        if (isNaN(numberOfLines) || numberOfLines <= 0  || numberOfLines > 3) {
+            console.log("invalid number of lines, try again dude!");
+        } else {
+            return numberOfLines;
+        }
+    }
+ };
+const depositAmount = deposit();
+console.log(`deposited amount: ${depositAmount}`)
+
+const numberOfLines = getNumberOfLines();
+console.log(`numberOfLines: ${numberOfLines}`);
